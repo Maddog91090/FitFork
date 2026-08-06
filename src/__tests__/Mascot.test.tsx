72c6dcd07cd61dd34fa97c6d2bc93d3ceff5811e
Alexandre PELLE <shaarky91@gmail.com>
2026-08-06 21:26:40 +0200

    feat: add Mascot component

---
+++ src/__tests__/Mascot.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import { Mascot, type MascotPose } from '../components/ui/Mascot';
+
+const POSES: MascotPose[] = ['idle', 'running', 'celebrating', 'encouraging'];
+
+describe('Mascot', () => {
+  it.each(POSES)('renders an image for the %s pose', async (pose) => {
+    const { getByTestId } = await render(<Mascot pose={pose} />);
+    expect(getByTestId('mascot-image')).toBeTruthy();
+  });
+
+  it('defaults to a size x size square when no style is given', async () => {
+    const { getByTestId } = await render(<Mascot pose="idle" size={96} />);
+    expect(getByTestId('mascot-image').props.style).toEqual({ width: 96, height: 96 });
+  });
+
+  it('lets an explicit style fully replace the default sizing', async () => {
+    const { getByTestId } = await render(
+      <Mascot pose="idle" style={{ width: '100%', aspectRatio: 2, maxHeight: 150 }} />
+    );
+    expect(getByTestId('mascot-image').props.style).toEqual({
+      width: '100%',
+      aspectRatio: 2,
+      maxHeight: 150,
+    });
+  });
+});
